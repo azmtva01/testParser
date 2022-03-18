4da8bfea0961b094dd157c52b89f40811d22b0e2
azmtva01 <azamatova0809@gmail.com>
2022-03-19 00:12:07 +0600

    rewrite commit

--- weatherProj/weatherApp/views.py
+++ weatherProj/weatherApp/views.py
@@ -1,18 +1,59 @@
 from django.shortcuts import render
-import requests
-from bs4 import BeautifulSoup
-
-# Getting articles from Habr
-
-toi_r = requests.get("https://habr.com/ru/all/")
-toi_soup = BeautifulSoup(toi_r.content, 'html5lib')
-toi_headings = toi_soup.find_all('h2')
-toi_headings = toi_headings[0:-13]
-toi_news = []
-
-for th in toi_headings:
-    toi_news.append(th.text)
 
 
-def index(req):
-    return render(req, 'weatherApp/home.html', {'toi_news': toi_news})
+def get_html_content(request):
+    import requests
+    city = request.GET.get('city')
+    city = city.replace(" ", "+")
+    USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
+    LANGUAGE = "en-US,en;q=0.5"
+    session = requests.Session()
+    session.headers['User-Agent'] = USER_AGENT
+    session.headers['Accept-Language'] = LANGUAGE
+    session.headers['Content-Language'] = LANGUAGE
+    html_content = session.get(f'https://www.google.com/search?q=weather+{city}').text
+    return html_content
+
+
+def home(request):
+    result = None
+    if 'city' in request.GET:
+        # fetch the weather from Google.
+        html_content = get_html_content(request)
+        from bs4 import BeautifulSoup
+        soup = BeautifulSoup(html_content, 'html.parser')
+        result = dict()
+        # extract region
+        result['region'] = soup.find("span", attrs={"class": "BNeawe tAd8D AP7Wnd"}).text
+        # extract temperature now
+        result['temp_now'] = soup.find("div", attrs={"class": "BNeawe iBp4i AP7Wnd"}).text
+        # get the day, hour and actual weather
+        result['dayhour'], result['weather_now'] = soup.find("div", attrs={"class": "BNeawe tAd8D AP7Wnd"}).text.split(
+            '\n')
+    return render(request, 'weatherApp/home.html', {'result': result})
+
+# import requests
+# from bs4 import BeautifulSoup
+# from django.shortcuts import render
+#
+#
+# def get_html_content(city):
+#     USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
+#     LANGUAGE = "en-US,en;q=0.5"
+#     session = requests.Session()
+#     session.headers['User-Agent'] = USER_AGENT
+#     session.headers['Accept-Language'] = LANGUAGE
+#     session.headers['Content-Language']  = LANGUAGE
+#     city = city.replace(' ', '+')
+#     html_content = session.get(f'https://www.google.com/search?q=weather+in+{city}').text
+#     return html_content
+#
+#
+# def home(request):
+#     if 'city' in request.GET:
+#         city = request.GET.get('city')
+#         html_content = get_html_content(city)
+#         soup = BeautifulSoup(html_content, 'html.parser')
+#         region = soup.find('div', attrs={'id': 'wob_loc'}).text
+#         print(region)
+#     return render(request, 'weatherApp/home.html')
