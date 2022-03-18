5c37aca2e26003eb2c6088310e83992ebd353260
azmtva01 <azamatova0809@gmail.com>
2022-03-19 00:02:21 +0600

    first commit

---
+++ weatherProj/weatherApp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
+
+# Getting articles from Habr
+
+toi_r = requests.get("https://habr.com/ru/all/")
+toi_soup = BeautifulSoup(toi_r.content, 'html5lib')
+toi_headings = toi_soup.find_all('h2')
+toi_headings = toi_headings[0:-13]
+toi_news = []
+
+for th in toi_headings:
+    toi_news.append(th.text)
+
+
+def index(req):
+    return render(req, 'weatherApp/home.html', {'toi_news': toi_news})
